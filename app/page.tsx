a0781db9b24ab4d14d0a648d9cd96962fe114011
Mohd. Nisab <nisabmohd@gmail.com>
2025-03-01 18:18:22 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Tweet from "@/components/tweet";
 
 export default function Home() {
   return (
-    <div className="max-w-[1200px] mx-auto py-8 flex lg:flex-row flex-col gap-12 px-5">
+    <div className="max-w-[1200px] mx-auto py-8 flex lg:flex-row flex-col gap-12 sm:px-5 px-2.5">
       <Editor />
       <Tweet />
     </div>
