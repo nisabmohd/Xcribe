cc26b976e4f9f25d8cda2bb9a4c567d20ba7118b
Mohd. Nisab <nisabmohd@gmail.com>
2025-03-01 18:18:45 +0530

    Update nav.tsx

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,7 +2,7 @@ import { BirdIcon } from "lucide-react";
 
 export default function Nav() {
   return (
-    <nav className="mx-auto max-w-[1200px] lg:h-36 h-28 flex flex-col justify-center px-5">
+    <nav className="mx-auto max-w-[1200px] lg:h-36 h-28 flex flex-col justify-center sm:px-5 px-2.5">
       <div className="flex gap-2 items-center">
         <BirdIcon />
         <h3 className="font-semibold text-lg">xcribe</h3>
